e93654c1f049fe001306d3a1d0a0bf5db026ff5f
Sebastian Friedrich <sebastian.friedrich.software@gmail.com>
2025-12-30 17:50:56 +0100

    fieat: make view_helper_value_to_enable_string more robust

--- MVC/View/view_helper_fcn/view_helper_value_to_enable_string.m
+++ MVC/View/view_helper_fcn/view_helper_value_to_enable_string.m
@@ -1,10 +1,13 @@
 function enableString = view_helper_value_to_enable_string(value)
 %VIEW_HELPER_VALUE_TO_ENABLE_STRING Summary of this function goes here
 
-if logical(value)
-    enableString = 'on';
-else
+try
+    if logical(value)
+        enableString = 'on';
+    else
+        enableString = 'off';
+    end
+catch 
+    warning('Error in %s','view_helper_value_to_enable_string')
     enableString = 'off';
-end
-
 end
