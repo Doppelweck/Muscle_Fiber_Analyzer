642c2e709b759351db3a89502a02a5a51ea1c148
KNUBA\sebas <sebastian.friedrich89@gmail.com>
2026-01-11 14:15:20 +0100

    feat: add new params default for grid layout

--- MVC/View/view_helper_fcn/view_helper_default_params.m
+++ MVC/View/view_helper_fcn/view_helper_default_params.m
@@ -14,12 +14,15 @@ else
     params.fontSizeB = 16; % Font size big
 end
 
-params.default_box_spacing_padding = {'Spacing',2,'Padding',2 };
-params.default_HButtonBox = {'ButtonSize', [600 20], 'Spacing', 2, 'Padding', 2};
-params.default_HButtonBox_Main = {'ButtonSize', [600 40], 'Spacing', 2, 'Padding', 2};
+clipping = 'on';
+
+params.default_box_spacing_padding = {'Spacing',2,'Padding',2,'Clipping',clipping };
+params.default_Grid_Flex_Buttons = {'Spacing',5,'Padding',5};
+params.default_HButtonBox = {'ButtonSize', [600 20], 'Spacing', 2, 'Padding', 2,'Clipping',clipping};
+params.default_HButtonBox_Main = {'ButtonSize', [600 40], 'Spacing', 2, 'Padding', 2,'Clipping',clipping};
 params.default_uiLabel = {'ButtonSize', [600 20], 'Spacing', 2, 'Padding', 2};
 params.default_normalized_font = {'FontUnits','normalized','Fontsize',0.5};
-params.default_panel = {'FontSize',params.fontSizeB,'BorderWidth',2,'Padding', 2, 'Tag','mainPanelsViews','HighlightColor',getHighlightColorValue()};
-params.default_tab_panel = {'FontSize',params.fontSizeM,'Padding', 2};
+params.default_panel = {'FontSize',params.fontSizeB,'BorderWidth',2,'Padding', 2,'Clipping',clipping, 'Tag','mainPanelsViews','HighlightColor',getHighlightColorValue()};
+params.default_tab_panel = {'FontSize',params.fontSizeM,'Padding', 2,'Clipping',clipping};
 
 end
